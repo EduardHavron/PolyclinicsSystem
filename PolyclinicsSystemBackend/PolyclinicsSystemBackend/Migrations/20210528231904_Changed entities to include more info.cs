998202c4cc3cd1eed9c29f5cdf9ff53af9e8c439
Kyrylo Stakhevych <kirill@magnificentsystems.com>
2021-05-29 02:21:34 +0300

    Added migration for new datetime field

---
+++ PolyclinicsSystemBackend/PolyclinicsSystemBackend/Migrations/20210528231904_Changed entities to include more info.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PolyclinicsSystemBackend.Migrations
+{
+    public partial class Changedentitiestoincludemoreinfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TreatmentDate",
+                table: "Treatments",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DiagnoseDate",
+                table: "Diagnoses",
+                type: "timestamp without time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TreatmentDate",
+                table: "Treatments");
+
+            migrationBuilder.DropColumn(
+                name: "DiagnoseDate",
+                table: "Diagnoses");
+        }
+    }
+}
